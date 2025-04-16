eba39f8e653c97a21a166fa2e0d8ced78df0e1e1
Divya Umapathy <divyaa.u.it.2021@snsct.org>
2025-04-16 14:20:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using WeatherApp;
+using MudBlazor.Services;
+using System.Net.Http;
+using Supabase;
+using Blazored.LocalStorage;
+
+
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
+
+
+//SUPABSE
+var supabaseUrl = "https://aklzdetoonchewhzrskx.supabase.co";
+var supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrbHpkZXRvb25jaGV3aHpyc2t4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1MzExMDYsImV4cCI6MjA1ODEwNzEwNn0.wlpZ8TJPvlNrsKQ9S47UQfLvNY2TSm-ohFB959Dg_b4";
+var supabaseOptions = new Supabase.SupabaseOptions();
+builder.Services.AddSingleton(new Supabase.Client(supabaseUrl, supabaseKey, supabaseOptions));
+
+
+await builder.Build().RunAsync();
+
+
---
+++ wwwroot/scripts/site.js
@@ -0,0 +1,34 @@
+﻿// Function to get the user's location
+window.getLocation = function () {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    resolve({
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    });
+                },
+                (error) => {
+                    reject(error.message);
+                }
+            );
+        } else {
+            reject("Geolocation is not supported by this browser.");
+        }
+    });
+};
+
+// Function to apply the theme based on the user's preference
+window.applyTheme = function (themeClass) {
+    document.body.className = themeClass;
+    localStorage.setItem("theme", themeClass); // Save theme preference
+};
+
+// Function to load the theme from localStorage on page load
+window.loadTheme = function () {
+    let savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+        document.body.className = savedTheme;
+    }
+};
