71747a5a319f0686c3625c5cdf9a75d955f0fd10
Divya Umapathy <divyaa.u.it.2021@snsct.org>
2025-05-13 13:07:30 +0530

    Added static folder for Render deployment

---
+++ static/Models.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+public class WeatherResponse
+{
+    public string Name { get; set; }
+    public MainData Main { get; set; }
+    public List<WeatherData> Weather { get; set; }
+}
+
+public class MainData
+{
+    public float Temp { get; set; }
+    public int Humidity { get; set; }
+}
+
+public class WeatherData
+{
+    public string Description { get; set; }
+}
+
+public class ForecastResponse
+{
+    public List<ForecastData> List { get; set; }
+}
+
+public class ForecastData
+{
+    [JsonPropertyName("dt")]
+    public long Dt { get; set; }
+    public MainData Main { get; set; }
+    public List<WeatherData> Weather { get; set; }
+
+    public DateTime DateTime => DateTimeOffset.FromUnixTimeSeconds(Dt).DateTime;
+}
